5cc676f56a0b67efadd1db6585fb464de760c55e
Josué Henrique <121066937+Mauboru@users.noreply.github.com>
2023-09-30 13:06:59 -0300

    buscar e alterar terminados

--- init.sql
+++ init.sql
@@ -1,16 +1,1 @@
-CREATE TABLE IF NOT EXISTS agenda (
-    codigo INT PRIMARY KEY AUTO_INCREMENT,
-    nome varchar(45) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS telefone (
-    telefone INT NOT NULL PRIMARY KEY,
-    codigo INT NOT NULL,
-    FOREIGN KEY (codigo) REFERENCES agenda(codigo)
-);
-
-CREATE TABLE IF NOT EXISTS email (
-    email varchar(100) NOT NULL PRIMARY KEY,
-    codigo INT NOT NULL,
-    FOREIGN KEY (codigo) REFERENCES agenda(codigo)
-);
+c
--- main.py
+++ main.py
@@ -16,11 +16,12 @@ conexao = mysql.connect(
 cursor = conexao.cursor(buffered=True)
 
 #Funções
-def listar():
-    comando = f'SELECT * FROM agenda'
-    cursor.execute(comando)
+def get_nome(cursor):
     resultado = cursor.fetchall()
-    print(resultado)
+    if resultado:
+        for linha in resultado:
+            nome = linha[0]
+    return nome
 
 def limpar():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -29,7 +30,6 @@ def executar(comando):
     cursor.execute(comando)
     conexao.commit()
 
-#Menu
 def exibir_menu():
     limpar()
 
@@ -38,12 +38,11 @@ def exibir_menu():
     print("===============================")
     print("1. Cadastrar")
     print("2. Buscar")
-    print("3. Atualizar Registro")
+    print("3. Alterar")
     print("4. Deletar Registro")
     print("5. Sair")
     print("===============================")
 
-# Função para cadastrar agenda, telefone e email
 def cadastrar():
     limpar()
     print("===============================")
@@ -78,7 +77,6 @@ def cadastrar():
             
         break
 
-# Função para ler registros
 def buscar():
     limpar()
     print("===============================")
@@ -87,54 +85,266 @@ def buscar():
     print('')
 
     while True:
-        nome = str(input('Digite o nome da agenda: '))
-        cursor.execute(f'SELECT codigo FROM agenda WHERE nome = "{nome}"')
-        id_agenda = cursor.fetchone()
+        pesquisa = str(input('Selecione qual tipo irá pesquisar:\n\n1 - Codigo\n2 - Nome\n3 - Telefone\n4 - Email\n\nQual sua escolha? '))           
+        if pesquisa == '1':
+            tipo = str(input('Digite o codigo: '))
+            cursor.execute(f'SELECT codigo FROM agenda WHERE codigo = {tipo}')
+            id_agenda = cursor.fetchone() #verificando se existe algo com esse valor
+            if id_agenda is None:
+                print('\nNão foi encontrado nenhuma resultado com essa busca!\n')
+                resposta = str(input('\nQuer buscar novamente?(s/n) '))
+                if resposta in 'Nn':
+                    break
+                else:
+                    continue     
 
-        if id_agenda is None:
-            print('\nNão foi encontrado nenhuma agenda com esse nome\n')
-            resposta = str(input('\nQuer buscar novamente?(s/n) '))
-            if resposta in 'Nn':
-                break
+            # Pegando o nome da AGENDA
+            cursor.execute(f'SELECT nome FROM agenda WHERE codigo = {tipo}') 
+            nome = get_nome(cursor)
+            print('\nAgenda:', nome)
+             
+            cursor.execute(f'SELECT * FROM telefone WHERE codigo = "{id_agenda[0]}"')
+            resultado = cursor.fetchall() 
+
+            if resultado:
+                print('\nTelefone(s):\n')
+                for linha in resultado:
+                    telefone = linha[0]
+                    print('- ', telefone)
             else:
-                continue
+                print('\nNão foi encontrado nenhum telefone associado a esta agenda')
 
-        cursor.execute(f'SELECT * FROM telefone WHERE codigo = "{id_agenda[0]}"')
-        resultado = cursor.fetchall()
+            cursor.execute(f'SELECT * FROM email WHERE codigo = "{id_agenda[0]}"')
+            resultado = cursor.fetchall()
 
-        print('\nAgenda:', nome)
-        if resultado:
-            print('\nTelefone(s):\n')
-            for linha in resultado:
-                telefone = linha[0]
-                print('- ', telefone)
-        else:
-            print('\nNão foi encontrado nenhum telefone associado a esta agenda')
+            if resultado:
+                print('\nEmail(s):\n')
+                for linha in resultado:
+                    email = linha[0]
+                    print('- ', email)
+            else:
+                print('\nNão foi encontrado nenhum email associado a esta agenda')
 
-        cursor.execute(f'SELECT * FROM email WHERE codigo = "{id_agenda[0]}"')
-        resultado = cursor.fetchall()
+        elif pesquisa == '2':
+            tipo = str(input('Digite o nome: '))
+            cursor.execute(f'SELECT codigo FROM agenda WHERE nome = "{tipo}"')
+            
+            id_agenda = cursor.fetchone()
+            if id_agenda is None:
+                print('\nNão foi encontrado nenhuma resultado com essa busca!\n')
+                resposta = str(input('\nQuer buscar novamente?(s/n) '))
+                if resposta in 'Nn':
+                    break
+                else:
+                    continue  
 
-        if resultado:
-            print('\nEmail(s):\n')
-            for linha in resultado:
-                email = linha[0]
-                print('- ', email)
-        else:
-            print('\nNão foi encontrado nenhum email associado a esta agenda')
+            # Pegando o nome da AGENDA
+            cursor.execute(f'SELECT codigo FROM agenda WHERE nome = "{tipo}"') 
+            codigo = get_nome(cursor)
+            print('\nAgenda:', tipo, ' / codigo(', codigo,')')
+            cursor.execute(f'SELECT * FROM telefone WHERE codigo = "{id_agenda[0]}"')
+            resultado = cursor.fetchall() 
+
+            if resultado:
+                print('\nTelefone(s):\n')
+                for linha in resultado:
+                    telefone = linha[0]
+                    print('- ', telefone)
+            else:
+                print('\nNão foi encontrado nenhum telefone associado a esta agenda')
+
+            cursor.execute(f'SELECT * FROM email WHERE codigo = "{id_agenda[0]}"')
+            resultado = cursor.fetchall()
+
+            if resultado:
+                print('\nEmail(s):\n')
+                for linha in resultado:
+                    email = linha[0]
+                    print('- ', email)
+            else:
+                print('\nNão foi encontrado nenhum email associado a esta agenda') 
+
+        elif pesquisa == '3':
+            tipo = str(input('Digite o telefone: '))
+            cursor.execute(f'SELECT codigo FROM telefone WHERE telefone  = {tipo}')
+            id_agenda = cursor.fetchone() #verificando se existe algo com esse valor
+            if id_agenda is None:
+                print('\nNão foi encontrado nenhuma resultado com essa busca!\n')
+                resposta = str(input('\nQuer buscar novamente?(s/n) '))
+                if resposta in 'Nn':
+                    break
+                else:
+                    continue     
+
+            # Pegando o nome da AGENDA
+            cursor.execute(f'SELECT nome FROM agenda WHERE codigo = {id_agenda[0]}') 
+            nome = get_nome(cursor)
+            print('\nAgenda:', nome)
+             
+            cursor.execute(f'SELECT * FROM telefone WHERE codigo = "{id_agenda[0]}"')
+            resultado = cursor.fetchall() 
+
+            if resultado:
+                print('\nTelefone(s):\n')
+                for linha in resultado:
+                    telefone = linha[0]
+                    print('- ', telefone)
+            else:
+                print('\nNão foi encontrado nenhum telefone associado a esta agenda')
+
+            cursor.execute(f'SELECT * FROM email WHERE codigo = "{id_agenda[0]}"')
+            resultado = cursor.fetchall()
+
+            if resultado:
+                print('\nEmail(s):\n')
+                for linha in resultado:
+                    email = linha[0]
+                    print('- ', email)
+            else:
+                print('\nNão foi encontrado nenhum email associado a esta agenda')
+        
+        elif pesquisa == '4':
+            tipo = str(input('Digite o email: '))
+            cursor.execute(f'SELECT codigo FROM email WHERE email  = "{tipo}"')
+            id_agenda = cursor.fetchone() #verificando se existe algo com esse valor
+            if id_agenda is None:
+                print('\nNão foi encontrado nenhuma resultado com essa busca!\n')
+                resposta = str(input('\nQuer buscar novamente?(s/n) '))
+                if resposta in 'Nn':
+                    break
+                else:
+                    continue     
+
+            # Pegando o nome da AGENDA
+            cursor.execute(f'SELECT nome FROM agenda WHERE codigo = {id_agenda[0]}') 
+            nome = get_nome(cursor)
+            print('\nAgenda:', nome)
+             
+            cursor.execute(f'SELECT * FROM telefone WHERE codigo = "{id_agenda[0]}"')
+            resultado = cursor.fetchall() 
+
+            if resultado:
+                print('\nTelefone(s):\n')
+                for linha in resultado:
+                    telefone = linha[0]
+                    print('- ', telefone)
+            else:
+                print('\nNão foi encontrado nenhum telefone associado a esta agenda')
+
+            cursor.execute(f'SELECT * FROM email WHERE codigo = "{id_agenda[0]}"')
+            resultado = cursor.fetchall()
+
+            if resultado:
+                print('\nEmail(s):\n')
+                for linha in resultado:
+                    email = linha[0]
+                    print('- ', email)
+            else:
+                print('\nNão foi encontrado nenhum email associado a esta agenda')
 
         resposta = str(input('\nQuer buscar novamente?(s/n) '))
         if resposta in 'Nn':
             break
 
-# Função para atualizar um registro
-def atualizar_registro():
-    limpar
-    print("Opção 3 - Atualizar Registro")
+def alterar():
+    limpar()
+    print("===============================")
+    print("       TELA  DE  EDIÇÃO        ")
+    print("===============================")
+    print('')
 
-# Função para deletar um registro
-def deletar_registro():
-    limpar
-    print("Opção 4 - Deletar Registro")
+    while True:
+        pesquisa = str(input('Digite o nome da agenda: '))
+
+        cursor.execute(f'SELECT codigo FROM agenda WHERE nome = "{pesquisa}"')
+        id_agenda = cursor.fetchone()
+        if id_agenda is None:
+            print('\nNão foi encontrado nenhuma resultado com essa busca!')
+            resposta = str(input('\nQuer buscar novamente?(s/n) '))
+            if resposta in 'Nn':
+                break
+            else:
+                continue 
+
+        acao = str(input('O que quer fazer?\n\n1 - Editar\n2 - Adicionar\n3 - Excluir\n\nDigite a ação: '))
+        if acao == '1':
+            tipo = str(input('O que vc quer editar?\n\n1 - Nome\n2 - Email\n3 - Telefone\n\nDigite a ação: '))
+
+            if tipo == '1':
+                nome = str(input('Digite o novo nome da agenda: '))
+                executar(f'UPDATE agenda SET nome = "{nome}" WHERE codigo = {id_agenda[0]}')
+                print('\nAgenda editada com sucesso!')
+
+            elif tipo == '2':
+                #listar os emails
+                email_antigo = str(input('Qual email quer editar? '))
+                cursor.execute(f'SELECT codigo FROM email WHERE email  = "{email_antigo}"')
+                id_agenda = cursor.fetchone()
+                if id_agenda is None:
+                    print('\nNão foi encontrado nenhuma resultado com essa busca!')
+                    resposta = str(input('\nQuer buscar novamente?(s/n) '))
+                    if resposta in 'Nn':
+                        break
+                    else:
+                        continue
+                email = str(input('Digite o novo email da agenda: '))
+                executar(f'UPDATE email SET email = "{email}" WHERE email = "{email_antigo}"')
+                print('\nEmail editado com sucesso!')
+
+            elif tipo == '3':
+                #listar os telefones
+                telefone_antigo = str(input('Qual telefone quer editar? '))
+                cursor.execute(f'SELECT codigo FROM telefone WHERE telefone  = "{telefone_antigo}"')
+                id_agenda = cursor.fetchone()
+                if id_agenda is None:
+                    print('\nNão foi encontrado nenhuma resultado com essa busca!')
+                    resposta = str(input('\nQuer buscar novamente?(s/n) '))
+                    if resposta in 'Nn':
+                        break
+                    else:
+                        continue
+                telefone = str(input('Digite o novo telefone da agenda: '))
+                executar(f'UPDATE telefone SET telefone = "{telefone}" WHERE telefone = "{telefone_antigo}"')
+                print('\nTelefone editado com sucesso!')
+
+        elif acao == '2':
+            tipo = str(input('O que vc quer adicionar?\n\n1 - Email\n2 - Telefone\n\nDigite a ação: '))
+
+            if tipo == '1':
+                email = str(input('Digite o novo email da agenda: '))
+                executar(f'INSERT INTO email (email, codigo) VALUES ("{email}", {id_agenda[0]})')
+                print('\nEmail adicionado com sucesso!')
+
+            elif tipo == '2':
+                telefone = str(input('Digite o novo telefone da agenda: '))
+                executar(f'INSERT INTO telefone (telefone, codigo) VALUES ("{telefone}", {id_agenda[0]})')
+                print('\nTelefone adicionado com sucesso!')
+
+        elif acao == '3':
+            tipo = str(input('O que vc quer excluir?\n\n1 - Email\n2 - Telefone\n\nDigite a ação: '))
+
+            if tipo == '1':
+                # listar emails
+                email = str(input('Digite o email: '))
+                executar(f'DELETE FROM email WHERE email = ("{email}")')
+                print('\nEmail deletado com sucesso!')
+
+            elif tipo == '2':
+                # listar telefones
+                telefone = str(input('Digite o telefone: '))
+                executar(f'DELETE FROM telefone WHERE telefone = ("{telefone}")')
+                print('\nTelefone deletado com sucesso!')
+
+        resposta = str(input('\nQuer editar novamente?(s/n) '))
+        if resposta in 'Nn':
+            break
+
+def deletar():
+    limpar()
+    print("===============================")
+    print("      TELA  DE  EXCLUSÃO       ")
+    print("===============================")
+    print('')
 
 # Loop principal do programa
 while True:
@@ -146,33 +356,14 @@ while True:
     elif opcao == "2":
         buscar()
     elif opcao == "3":
-        atualizar_registro()
+        alterar()
     elif opcao == "4":
-        deletar_registro()
+        deletar()
     elif opcao == "5":
         print("Saindo do programa. Até logo!")
         break
     else:
         input("Opção inválida. Pressione Enter para continuar...")
-
-#Update
-resposta = str(input('Quer editar? '))
-while not resposta in 'Nn':
-    nome = str(input('Digite o nome na agenda para editar: '))
-    novo_nome = str(input('Digite o novo nome: '))
-    comando = f'UPDATE agenda SET nome = "{novo_nome}" WHERE nome = "{nome}"'
-    cursor.execute(comando)
-    conexao.commit()
-    resposta = str(input('Quer editar novamente? '))
-
-#Excluir
-resposta = str(input('Quer excluir? '))
-while not resposta in 'Nn':
-    nome = str(input('Digite o nome na agenda para deletar: '))
-    comando = f'DELETE FROM agenda WHERE nome = "{nome}"'
-    cursor.execute(comando)
-    conexao.commit()
-    resposta = str(input('Quer excluir novamente? '))
-
+        
 cursor.close()
 conexao.close()
