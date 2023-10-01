163911fd32ca50679d2e7a540c88d3451cd26f8a
Josué Henrique <121066937+Mauboru@users.noreply.github.com>
2023-10-01 00:43:07 -0300

    corrigindo erros

--- init.sql
+++ init.sql
@@ -1,1 +1,16 @@
-c
+CREATE TABLE IF NOT EXISTS agenda(
+    codigo INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL,
+)
+
+CREATE TABLE IF NOT EXISTS telefone(
+    telefone INT NOT NULL PRIMARY KEY,
+    codigo INT NOT NULL,
+    FOREIGN KEY (codigo) REFERENCES agenda(codigo)
+)
+
+CREATE TABLE IF NOT EXISTS email(
+    email VARCHAR(100) NOT NULL PRIMARY KEY,
+    codigo INT NOT NULL,
+    FOREIGN KEY (codigo) REFERENCES agenda(codigo)
+)
