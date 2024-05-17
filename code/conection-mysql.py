54613bfe9ca6161af5325d0fe84754835cb1daf5
mauboru <josue21henrique@gmail.com>
2024-05-17 19:24:03 -0300

    try catch buscar

--- code/conection-mysql.py
+++ code/conection-mysql.py
@@ -22,7 +22,7 @@ while True:
     if opcao == "1":
         functions.cadastrar(cursor, conexao)
     elif opcao == "2":
-        functions.buscar()
+        functions.buscar(cursor, conexao)
     elif opcao == "3":
         functions.alterar()
     elif opcao == "4":
@@ -33,24 +33,6 @@ while True:
     else:
         input("Opção inválida. Pressione Enter para continuar...")
 
-
-# def get_nome(cursor):
-#     resultado = cursor.fetchall()
-#     if resultado:
-#         for linha in resultado:
-#             nome = linha[0]
-#     return nome
-
-# def verifica_id(id):
-#     if id is None:
-#         print('\nNão foi encontrado nenhum resultado com essa busca!')
-#         resposta = input('\nQuer buscar novamente? (s/n) ')
-        
-#         if resposta in 'Nn':
-#             return 'sair'
-#         else:
-#             return 'refazer'
-
 # def listar(tabela, id):
 #     cursor.execute(f'SELECT * FROM {tabela} WHERE codigo = {id}')
 #     resultado = cursor.fetchall()
@@ -59,166 +41,6 @@ while True:
 #         for linha in resultado:
 #             print(linha[0])
 #         print('')
-
-# def buscar():
-#     os.system('cls' if os.name == 'nt' else 'clear')
-#     print("===============================")
-#     print(f"       {Fore.CYAN}TELA DE PESQUISA{Fore.RESET}        ")
-#     print("===============================\n")
-
-#     while True:
-#         pesquisa = str(input('Selecione por qual tipo irá pesquisar:\n\n1 - Codigo\n2 - Nome\n3 - Telefone\n4 - Email\n\nQual sua escolha? '))           
-        
-#         if pesquisa == '1':
-#             tipo = str(input('Digite o codigo: '))
-#             cursor.execute(f'SELECT codigo FROM agenda WHERE codigo = {tipo}')
-            
-#             id = cursor.fetchone()
-#             resultado = verifica_id(id)
-#             if resultado == 'sair':
-#                 break
-#             elif resultado == 'refazer':
-#                 continue
-                
-#             # Pegando o nome da AGENDA
-#             cursor.execute(f'SELECT nome FROM agenda WHERE codigo = {tipo}') 
-#             nome = get_nome(cursor)
-#             print('\nAgenda:', nome)
-#             print('Codigo:', tipo)
-
-#             cursor.execute(f'SELECT * FROM telefone WHERE codigo = "{id[0]}"')
-#             resultado = cursor.fetchall() 
-#             if resultado:
-#                 print('\nTelefone(s):\n')
-#                 for linha in resultado:
-#                     telefone = linha[0]
-#                     print('- ', telefone)
-#             else:
-#                 print('\nNão foi encontrado nenhum telefone associado a esta agenda')
-
-#             cursor.execute(f'SELECT * FROM email WHERE codigo = "{id[0]}"')
-#             resultado = cursor.fetchall()
-#             if resultado:
-#                 print('\nEmail(s):\n')
-#                 for linha in resultado:
-#                     email = linha[0]
-#                     print('- ', email)
-#             else:
-#                 print('\nNão foi encontrado nenhum email associado a esta agenda')
-
-#         elif pesquisa == '2':
-#             tipo = str(input('Digite o nome: '))
-#             cursor.execute(f'SELECT codigo FROM agenda WHERE nome = "{tipo}"')
-            
-#             id = cursor.fetchone()
-#             resultado = verifica_id(id)
-#             if resultado == 'sair':
-#                 break
-#             elif resultado == 'refazer':
-#                 continue
-
-#             # Pegando o nome da AGENDA
-#             cursor.execute(f'SELECT codigo FROM agenda WHERE nome = "{tipo}"') 
-#             codigo = get_nome(cursor)
-#             print('\nAgenda:', tipo)
-#             print('Codigo:', codigo)
-#             cursor.execute(f'SELECT * FROM telefone WHERE codigo = "{id[0]}"')
-
-#             resultado = cursor.fetchall() 
-#             if resultado:
-#                 print('\nTelefone(s):\n')
-#                 for linha in resultado:
-#                     telefone = linha[0]
-#                     print('- ', telefone)
-#             else:
-#                 print('\nNão foi encontrado nenhum telefone associado a esta agenda')
-
-#             cursor.execute(f'SELECT * FROM email WHERE codigo = "{id[0]}"')
-#             resultado = cursor.fetchall()
-#             if resultado:
-#                 print('\nEmail(s):\n')
-#                 for linha in resultado:
-#                     email = linha[0]
-#                     print('- ', email)
-#             else:
-#                 print('\nNão foi encontrado nenhum email associado a esta agenda') 
-
-#         elif pesquisa == '3':
-#             tipo = str(input('Digite o telefone: '))
-#             cursor.execute(f'SELECT codigo FROM telefone WHERE telefone  = {tipo}')
-
-#             id = cursor.fetchone()
-#             resultado = verifica_id(id)
-#             if resultado == 'sair':
-#                 break
-#             elif resultado == 'refazer':
-#                 continue   
-
-#             # Pegando o nome da AGENDA
-#             cursor.execute(f'SELECT nome FROM agenda WHERE codigo = {id[0]}') 
-#             nome = get_nome(cursor)
-#             print('\nAgenda:', nome)
-#             print('Codigo:', codigo)
-             
-#             cursor.execute(f'SELECT * FROM telefone WHERE codigo = "{id[0]}"')
-#             resultado = cursor.fetchall() 
-#             if resultado:
-#                 print('\nTelefone(s):\n')
-#                 for linha in resultado:
-#                     telefone = linha[0]
-#                     print('- ', telefone)
-#             else:
-#                 print('\nNão foi encontrado nenhum telefone associado a esta agenda')
-
-#             cursor.execute(f'SELECT * FROM email WHERE codigo = "{id[0]}"')
-#             resultado = cursor.fetchall()
-#             if resultado:
-#                 print('\nEmail(s):\n')
-#                 for linha in resultado:
-#                     email = linha[0]
-#                     print('- ', email)
-#             else:
-#                 print('\nNão foi encontrado nenhum email associado a esta agenda')
-        
-#         elif pesquisa == '4':
-#             tipo = str(input('Digite o email: '))
-#             cursor.execute(f'SELECT codigo FROM email WHERE email  = "{tipo}"')
-
-#             id = cursor.fetchone()
-#             resultado = verifica_id(id)
-#             if resultado == 'sair':
-#                 break
-#             elif resultado == 'refazer':
-#                 continue
-
-#             cursor.execute(f'SELECT nome FROM agenda WHERE codigo = {id[0]}') 
-#             nome = get_nome(cursor)
-#             print('\nAgenda:', nome)
-#             print('Codigo:', codigo)
-             
-#             cursor.execute(f'SELECT * FROM telefone WHERE codigo = "{id[0]}"')
-#             resultado = cursor.fetchall() 
-#             if resultado:
-#                 print('\nTelefone(s):\n')
-#                 for linha in resultado:
-#                     telefone = linha[0]
-#                     print('- ', telefone)
-#             else:
-#                 print('\nNão foi encontrado nenhum telefone associado a esta agenda')
-
-#             cursor.execute(f'SELECT * FROM email WHERE codigo = "{id[0]}"')
-#             resultado = cursor.fetchall()
-#             if resultado:
-#                 print('\nEmail(s):\n')
-#                 for linha in resultado:
-#                     email = linha[0]
-#                     print('- ', email)
-#             else:
-#                 print('\nNão foi encontrado nenhum email associado a esta agenda')
-
-#         resposta = str(input('\nQuer buscar novamente?(s/n) '))
-#         if resposta in 'Nn':
-#             break
 
 # def alterar():
 #     os.system('cls' if os.name == 'nt' else 'clear')
--- code/functions.py
+++ code/functions.py
@@ -17,6 +17,23 @@ def executar(comando, cursor, conexao):
     cursor.execute(comando)
     conexao.commit()
 
+def get_nome(cursor):
+    resultado = cursor.fetchall()
+    if resultado:
+        for linha in resultado:
+            nome = linha[0]
+    return nome
+
+def verifica_id(id):
+    if id is None:
+        print('\nNão foi encontrado nenhum resultado com essa busca!')
+        resposta = input('\nQuer buscar novamente? (s/n) ')
+        
+        if resposta in 'Nn':
+            return 'sair'
+        else:
+            return 'refazer'
+
 def cadastrar(cursor, conexao):
     os.system('cls' if os.name == 'nt' else 'clear')
     print("===============================")
@@ -49,3 +66,167 @@ def cadastrar(cursor, conexao):
             print(f"{Fore.RED}Ocorreu um erro durante o cadastro: {e}{Fore.RESET}")
             conexao.rollback()
             continue
+
+def buscar(cursor, conexao):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("===============================")
+    print(f"       {Fore.CYAN}TELA DE PESQUISA{Fore.RESET}        ")
+    print("===============================\n")
+
+    while True:
+        try:
+            pesquisa = str(input('Selecione por qual tipo irá pesquisar:\n\n1 - Codigo\n2 - Nome\n3 - Telefone\n4 - Email\n\nQual sua escolha? '))           
+
+            if pesquisa == '1':
+                tipo = str(input('Digite o codigo: '))
+                cursor.execute(f'SELECT codigo FROM agenda WHERE codigo = {tipo}')
+                
+                id = cursor.fetchone()
+                resultado = verifica_id(id)
+                if resultado == 'sair':
+                    break
+                elif resultado == 'refazer':
+                    continue
+                    
+                # Pegando o nome da AGENDA
+                cursor.execute(f'SELECT nome FROM agenda WHERE codigo = {tipo}') 
+                nome = get_nome(cursor)
+                print('\nAgenda:', nome)
+                print('Codigo:', tipo)
+
+                cursor.execute(f'SELECT * FROM telefone WHERE codigo = "{id[0]}"')
+                resultado = cursor.fetchall() 
+                if resultado:
+                    print('\nTelefone(s):\n')
+                    for linha in resultado:
+                        telefone = linha[0]
+                        print('- ', telefone)
+                else:
+                    print('\nNão foi encontrado nenhum telefone associado a esta agenda')
+
+                cursor.execute(f'SELECT * FROM email WHERE codigo = "{id[0]}"')
+                resultado = cursor.fetchall()
+                if resultado:
+                    print('\nEmail(s):\n')
+                    for linha in resultado:
+                        email = linha[0]
+                        print('- ', email)
+                else:
+                    print('\nNão foi encontrado nenhum email associado a esta agenda')
+
+            elif pesquisa == '2':
+                tipo = str(input('Digite o nome: '))
+                cursor.execute(f'SELECT codigo FROM agenda WHERE nome = "{tipo}"')
+                
+                id = cursor.fetchone()
+                resultado = verifica_id(id)
+                if resultado == 'sair':
+                    break
+                elif resultado == 'refazer':
+                    continue
+
+                # Pegando o nome da AGENDA
+                cursor.execute(f'SELECT codigo FROM agenda WHERE nome = "{tipo}"') 
+                codigo = get_nome(cursor)
+                print('\nAgenda:', tipo)
+                print('Codigo:', codigo)
+                cursor.execute(f'SELECT * FROM telefone WHERE codigo = "{id[0]}"')
+
+                resultado = cursor.fetchall() 
+                if resultado:
+                    print('\nTelefone(s):\n')
+                    for linha in resultado:
+                        telefone = linha[0]
+                        print('- ', telefone)
+                else:
+                    print('\nNão foi encontrado nenhum telefone associado a esta agenda')
+
+                cursor.execute(f'SELECT * FROM email WHERE codigo = "{id[0]}"')
+                resultado = cursor.fetchall()
+                if resultado:
+                    print('\nEmail(s):\n')
+                    for linha in resultado:
+                        email = linha[0]
+                        print('- ', email)
+                else:
+                    print('\nNão foi encontrado nenhum email associado a esta agenda') 
+
+            elif pesquisa == '3':
+                tipo = str(input('Digite o telefone: '))
+                cursor.execute(f'SELECT codigo FROM telefone WHERE telefone  = {tipo}')
+
+                id = cursor.fetchone()
+                resultado = verifica_id(id)
+                if resultado == 'sair':
+                    break
+                elif resultado == 'refazer':
+                    continue   
+
+                # Pegando o nome da AGENDA
+                cursor.execute(f'SELECT nome FROM agenda WHERE codigo = {id[0]}') 
+                nome = get_nome(cursor)
+                print('\nAgenda:', nome)
+                
+                cursor.execute(f'SELECT * FROM telefone WHERE codigo = "{id[0]}"')
+                resultado = cursor.fetchall() 
+                if resultado:
+                    print('\nTelefone(s):\n')
+                    for linha in resultado:
+                        telefone = linha[0]
+                        print('- ', telefone)
+                else:
+                    print('\nNão foi encontrado nenhum telefone associado a esta agenda')
+
+                cursor.execute(f'SELECT * FROM email WHERE codigo = "{id[0]}"')
+                resultado = cursor.fetchall()
+                if resultado:
+                    print('\nEmail(s):\n')
+                    for linha in resultado:
+                        email = linha[0]
+                        print('- ', email)
+                else:
+                    print('\nNão foi encontrado nenhum email associado a esta agenda')
+            
+            elif pesquisa == '4':
+                tipo = str(input('Digite o email: '))
+                cursor.execute(f'SELECT codigo FROM email WHERE email  = "{tipo}"')
+
+                id = cursor.fetchone()
+                resultado = verifica_id(id)
+                if resultado == 'sair':
+                    break
+                elif resultado == 'refazer':
+                    continue
+
+                cursor.execute(f'SELECT nome FROM agenda WHERE codigo = {id[0]}') 
+                nome = get_nome(cursor)
+                print('\nAgenda:', nome)
+                
+                cursor.execute(f'SELECT * FROM telefone WHERE codigo = "{id[0]}"')
+                resultado = cursor.fetchall() 
+                if resultado:
+                    print('\nTelefone(s):\n')
+                    for linha in resultado:
+                        telefone = linha[0]
+                        print('- ', telefone)
+                else:
+                    print('\nNão foi encontrado nenhum telefone associado a esta agenda')
+
+                cursor.execute(f'SELECT * FROM email WHERE codigo = "{id[0]}"')
+                resultado = cursor.fetchall()
+                if resultado:
+                    print('\nEmail(s):\n')
+                    for linha in resultado:
+                        email = linha[0]
+                        print('- ', email)
+                else:
+                    print('\nNão foi encontrado nenhum email associado a esta agenda')
+
+            resposta = str(input('\nQuer buscar novamente?(s/n) '))
+            if resposta in 'Nn':
+                break
+        except Exception as e:
+            print(f"{Fore.RED}Ocorreu um erro durante o cadastro: {e}{Fore.RESET}")
+            conexao.rollback()
+            continue
+
