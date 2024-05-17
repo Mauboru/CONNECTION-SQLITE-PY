20f7b0fef89ecf0a53d27776749d71dbb673328e
mauboru <josue21henrique@gmail.com>
2024-05-17 19:44:15 -0300

    update slite db

--- code/conection-sqlite.py
+++ code/conection-sqlite.py
@@ -1,15 +1,28 @@
 import sqlite3 as sql
 import dotenv as env
 from colorama import Fore
-import os, main
+import os, functions
 
 conexao = sql.connect('sqlite.db')
 cursor = conexao.cursor()
 
-# Executa o comando para executar
-def executar(comando):
-    cursor.execute(comando)
-    conexao.commit()
+while True:
+    functions.exibir_menu()
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+        functions.cadastrar(cursor, conexao)
+    elif opcao == "2":
+        functions.buscar(cursor, conexao)
+    elif opcao == "3":
+        functions.alterar(cursor, conexao)
+    elif opcao == "4":
+        functions.deletar(cursor, conexao)
+    elif opcao == "5":
+        print("Saindo do programa. Até logo!")
+        break
+    else:
+        input("Opção inválida. Pressione Enter para continuar...")
 
 cursor.close()
 conexao.close()
--- data/init.sql
+++ data/init.sql
@@ -1,21 +1,23 @@
 -- SQLITE
 
-CREATE TABLE IF NOT EXISTS agenda(
-    codigo INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(45) NOT NULL,
-)
+CREATE TABLE IF NOT EXISTS agenda (
+    codigo INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(45) NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS telefone(
-    telefone INT NOT NULL PRIMARY KEY,
-    codigo INT NOT NULL,
+CREATE TABLE IF NOT EXISTS telefone (
+    telefone VARCHAR(15) NOT NULL,
+    codigo INTEGER NOT NULL,
+    PRIMARY KEY (telefone),
     FOREIGN KEY (codigo) REFERENCES agenda(codigo)
-)
+);
 
-CREATE TABLE IF NOT EXISTS email(
-    email VARCHAR(100) NOT NULL PRIMARY KEY,
-    codigo INT NOT NULL,
+CREATE TABLE IF NOT EXISTS email (
+    email VARCHAR(100) NOT NULL,
+    codigo INTEGER NOT NULL,
+    PRIMARY KEY (email),
     FOREIGN KEY (codigo) REFERENCES agenda(codigo)
-)
+);
 
 -- MYSQL
 
